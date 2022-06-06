6301a4f776e4d7e9318a730514e955785849cc1c
Jackson Dean <Jackson.Dean@uvm.edu>
2022-06-06 16:13:34 -0400

    fix typos and remove matplot lib auto-installation

--- lesson-1/1-intro.py
+++ lesson-1/1-intro.py
@@ -4,6 +4,6 @@
 # They can be used to make your code more readable.
 
 # Below is the first line of code. Lines of code are interpreted by the computer in the order they appear.
-# Replace the line of code below with your own code to display "Hello World!".
+# Replace the line of code below with your own code to display "Hello, World!".
 
 ... # replace this line!
--- lesson-1/2-variables.py
+++ lesson-1/2-variables.py
@@ -5,13 +5,13 @@
 # strings are sequences of characters, enclosed in quotes.
 
 # This is a string. Try changing it's stored value. 
-statement_to_print = "Hello World!" 
+statement_to_print = "Hello, World!" 
 
 print(statement_to_print)
 
 # You can do "operations" on variables.
 # For example, you can add two or more variables together.
-first_word = "Hello"
+first_word = "Hello,"
 second_word = "World!"
 statement_to_print = first_word + " " + second_word
 print(statement_to_print)
